1a7c392c47a7844e07d57fe8c067ab59fe75e373
mchien2002 <minhchien77777@gmail.com>
2022-12-16 21:20:08 +0700

    pass update

--- src/entities/account.go
+++ src/entities/account.go
@@ -11,11 +11,12 @@ type Account struct {
 }
 
 type AccountWithToken struct {
-	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Role     int    `json:"role"`
-	State    int    `json:"state"`
-	Token    string `json:"token"`
+	ID         int    `json:"id"`
+	Email      string `json:"email"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	Role       int    `json:"role"`
+	State      int    `json:"state"`
+	Token      string `json:"token"`
+	Created_at string `json:"created_at"`
 }
